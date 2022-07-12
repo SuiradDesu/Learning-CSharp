8c2346288e6bc213a7b6ce266e41355055c71012
suiraddesu <luckcastle@outlook.com>
2022-07-12 13:02:02 -0300

    initial commit

---
+++ module01/variaveis/Program.cs
@@ -0,0 +1,11 @@
+﻿// Variáveis em C#
+
+bool vf = true;
+char caracteres = 'C';
+byte barramento = 64;
+int numero = 12345678;
+long numerosGrandes = 81277361237712637;
+float fracoes = 5.12f; // usamos o sufíxo F para o tipo float.
+double pontoFlutuantePadrao = 4.55;
+decimal moedas = 5.543m; // Usamos o sufíxo M para o tipo decimal.
+string letras = "Agora aqui só vai textos";
